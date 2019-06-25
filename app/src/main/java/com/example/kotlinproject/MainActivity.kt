139b9261a6772b06562b27f9d44694af84618224
Deepak Sharma <9928532714>
2019-06-25 08:54:14 +0530

    first commit

---
+++ app/src/main/java/com/example/kotlinproject/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        ViewModelProviders.of(this).get(MainActivity.class);
+//ViewModelProviders.of(this).get(MainActivity.class)
+        setContentView(R.layout.activity_main)
+
+
+    
+}
+
+
+}

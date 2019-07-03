e21ba8f0508f07ceb0c20ac511efb3b6cd0bda4b
Deepak Sharma <9928532714>
2019-07-03 08:57:10 +0530

    api call

--- app/src/main/java/com/example/kotlinproject/apiUtils/ApiServices.kt
+++ app/src/main/java/com/example/kotlinproject/apiUtils/ApiServices.kt
@@ -1,6 +1,5 @@
 package com.prodege.shopathome.model.networkCall
 
-import com.example.kotlinproject.model.github.Project
 import com.example.kotlinproject.model.respo.newsChannel.NewsChanelRespo
 import kotlinx.coroutines.Deferred
 import retrofit2.Call
@@ -8,12 +7,8 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface ApiServices {
-    @GET("users/{user}/repos")
-    fun getProjectList(@Path("user") user: String): Deferred<Response<Project>>//P: Call<List<Project>>
     @GET
-    abstract fun getChannelListName(@Url strUrl: String): Deferred<Response<NewsChanelRespo>>//Call<CategoryDetails>
+     fun getChannelListName(@Url strUrl: String): Deferred<Response<NewsChanelRespo>>//Call<CategoryDetails>
 
-    @GET("/repos/{user}/{reponame}")
-    fun getProjectDetails(@Path("user") user: String, @Path("reponame") projectName: String): Call<Project>
 
 }

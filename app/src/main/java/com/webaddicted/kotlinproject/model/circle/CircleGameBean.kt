a1d5ddb663eede0981f004acfc9458a92d09a64a
Deepak Sharma <38448422+webaddicted@users.noreply.github.com>
2020-01-09 14:32:01 +0530

    Update CircleGameBean.kt

--- app/src/main/java/com/webaddicted/kotlinproject/model/circle/CircleGameBean.kt
+++ app/src/main/java/com/webaddicted/kotlinproject/model/circle/CircleGameBean.kt
@@ -1,10 +1,12 @@
 package com.webaddicted.kotlinproject.model.circle
 
-open class CircleGameBean {
+open class CircleGameBean : Comparator<CircleGameBean> {
     var id:String = ""
     var gameName:String = ""
     var gameImg:String = ""
-
+  override fun compare(m1: CircleGameBean?, m2: CircleGameBean?): Int {
+        return m1?.gameName?.compareTo(m2?.gameName.toString())!!
+    }
 //    constructor() : this()
 //    constructor() : this()
 
